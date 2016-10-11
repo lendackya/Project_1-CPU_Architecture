aeaec75ece3cfb71a91b507415683ec16e014512
lendackya <lendackya@gmail.com>
2016-10-10 20:32:41 -0400

    WIP

--- HazardControl.h
+++ HazardControl.h
@@ -12,17 +12,49 @@ bool DATA_HAZARD_FLG_TWO = false;
 bool DATA_HAZARD_FLG_THREE = false; 
 bool CONTROL_HAZARD_FLG = false; 
 
+// Checks if the instruction in the given stage is a 'Write' instruction
+bool will_write_to_reg(int stage){
+	
+	if (strcmp(pipeline[stage].type, "RTYPE") == 0 || strcmp(pipeline[stage].type, "LOAD") == 0 || strcmp(pipeline[stage].type, "ITYPE") == 0)){ return true; }
+		
+	return false; 
+}
+
+// Checks if the instuction in the given stage is a 'Read' instruction
+bool will_read_from_reg(int stage){
+
+	if (strcmp(pipeline[stage].type, "RTYPE") == 0 || strcmp(pipeline[stage].type, "ITYPE") == 0 || strcmp(pipeline[stage].type, "BRANCH") == 0)){ return true; }
+	
+	return false; 
+}
+
+// Checks if the instruction in the given stage will read from memory
+bool will_read_from_memory(int stage){
+	
+	if (strcmp(pipeline[stage].type, "LOAD") == 0 ){ return true; }
+	
+	return false; 	
+}
+
+// Checks if the instruction in the given stage will write to memory 
+bool will_write_to_memory(int stage){
+	
+	if (strcmp(pipeline[stage].type, "STORE") == 0 ){ return true; }
+	
+	return false; 	
+}
 
 void check_hazards(){
 	
-	
 	// Data Hazards: 
 		
-		if (strcmp(pipeline[0].type, "")){
+		if (will_write_to_reg(0) && will_read_from_reg(2)){
 			
-			DATA_HAZARD_FLG_ONE = true; 
+			if (pipeline[0].rd == pipelin[2].rd){
+				DATA_HAZARD_FLG_ONE = true; 
+			}
 			
-		}else if ( (strcmp(pipelin[4].type, "LOAD") == 0) && (strcmp(pipeline[2].type, "READ") == 0) ){
+		}else if ( pipeline[4].type == ti_LOAD && (strcmp(pipeline[2].type, "READ") == 0) ){
 			
 			if (pipeline[4].rt == pipeline[0].rt ){ DATA_HAZARD_FLG_TWO = true; } // set flag
 			
@@ -49,7 +81,6 @@ void check_hazards(){
 				CONTROL_HAZARD_FLG = true;  			
 		}
 }
-
 
 // Take neccessay action if the flag is true
 void avoid_hazards(){
